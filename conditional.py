4a9800399c90971ad68adbdca5a5d9f7967b1131
Sandhya015 <sandhyanaik3249@gmail.com>
2024-09-19 18:47:10 +0530

    codechef-qstns

---
+++ conditional.py
@@ -0,0 +1,8 @@
+# cook your dish here
+H=int(input("enter the hours"))
+if (H<8):
+    print("less")
+elif(H==8):
+    print("perfect")
+elif (H>8):
+    print("more")
---
+++ pages.py
@@ -0,0 +1,11 @@
+#Rahul has a 5 5-page assignment due in 6060 minutes. He can write one page in 
+#X minutes.Determine if Rahul can complete the assignment within the given time constraint.
+
+# cook your dish here
+x=int(input())
+time=(5*x)
+
+if(time<=60):
+    print("yes")
+else:
+    print("no")
